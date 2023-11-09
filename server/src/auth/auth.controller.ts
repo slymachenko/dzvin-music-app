e0c90c73dbd5d6e32a1cc50279c7ea88b85a6dfe
Arsenii Bryndin <senya.s408@gmail.com>
2023-11-09 11:26:09 +0100

    signup,signin init

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
-@Controller()
+@Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup(@Body() dto: AuthDto) {
+    return this.authService.signup(dto);
+  }
+
+  @Post('signin')
+  signin(@Body() dto: AuthDto) {
+    return this.authService.signup(dto);
+  }
 }
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
-@Injectable({})
-export class AuthService {}
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  signup(dto: AuthDto) {
+    return { msg: 'SIGN UP', dto };
+  }
+
+  signin(dto: AuthDto) {
+    return { msg: 'SIGN IN', dto };
+  }
+}
